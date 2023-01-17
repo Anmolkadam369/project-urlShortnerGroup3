fae370216c82d21ca7600377d3679aca81607aa5
anmol <anmolkadam369@gmail.com>
2023-01-17 23:15:07 +0530

    update

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -2,54 +2,42 @@ const urlModel = require("../model/urlModel")
 const shortid = require('shortid');
 const isValid = require("../validator/validation")
 
-
 const urlCreate = async function (req, res) {
-
     try {
         let data = req.body;
         if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "No data in body" });
-        if (!data.longUrl) return res.status(400).send({ status: false, message: "longUrl is required" });
-        let dummyUrl = data.longUrl;
-        dummyUrl = data.longUrl = dummyUrl.trim()
-        if (!isValid.isValidLink(data.longUrl)) return res.status(400).send({ status: false, message: "longUrl is not valid" });
-        let presentInDataBase = await urlModel.findOne({ longUrl: data.longUrl });
-        console.log(presentInDataBase)
-        if (presentInDataBase != null) return res.status(200).send({ msg: "shortUrl is Already Generated", data: presentInDataBase });
+        if (Object.keys(data).length > 1) return res.status(400).send({ status: false, message: "Only one data can send from body" })
+        if (Object.keys(data) != "longUrl") return res.status(400).send({ status: false, message: "longUrl is required" });
+
+        data.longUrl = data.longUrl.trim()
+        if (Object.values(data.longUrl) == "") return res.status(400).send({ status: false, message: "No values provided" })
+        if (!isValid.isValidLink(data.longUrl)) return res.status(400).send({ status: false, message: "Please provide valid longUrl" })
+
+        let presentInDataBase = await urlModel.findOne({ longUrl: data.longUrl }).select({ _id: 0, __v: 0 });
+        if (presentInDataBase !== null) return res.status(200).send({ message: "shortUrl is Already Generated", data: presentInDataBase });
+
         let baseUrl = "https://localhost:3000/";
         let urlCode = shortid.generate();
         var shortUrl = baseUrl.concat(urlCode);
-        let newData = {
-            longUrl: data.longUrl,
-            shortUrl: shortUrl,
-            urlCode: urlCode
-        };
-        let finalData = await urlModel.create(newData);
-        res.status(201).send({ data: finalData });
 
-        req.urlCode=urlCode;
-        console.log(req.urlCode)
+        let newData = { longUrl: data.longUrl, shortUrl: shortUrl, urlCode: urlCode };
+        let createdData = await urlModel.create(newData);
+        let finalData = await urlModel.findOne({ urlCode: createdData.urlCode }).select({ _id: 0, __v: 0 })
+        res.status(201).send({ data: finalData });
     }
-    
     catch (error) { res.status(500).send({ error: error.message }); }
 }
 
 const urlGet = async function (req, res) {
     try {
-        console.log(req.urlCode)
-
         let data = req.params.urlCode;
-        if (data.length !== 9) return res.status(400).send({ status: false, msg: "Not valid" })
+        if (!shortid.isValid(data) || data.length !== 9) return res.status(400).send({ status: false, msg: "Not valid urlCode" })
         let ans = await urlModel.findOne({ urlCode: data });
         if (ans === null) return res.status(404).send({ status: false, msg: "not found with given data" })
-        res.status(302).send(`found.Redirecting to ${ans.longUrl}`)
+        // res.status(302).send(`found.Redirecting to ${ans.longUrl}`)
+        res.status(302).redirect(ans.longUrl)
     }
     catch (err) { res.status(500).send({ status: false, err: err.message }) }
 }
 
-
-
-
-
-
-module.exports.urlCreate = urlCreate;
-module.exports.urlGet = urlGet;
+module.exports = { urlCreate, urlGet };
--- src/validator/validation.js
+++ src/validator/validation.js
@@ -1,12 +1,6 @@
 
-const isValidLink = function(longUrl){
-    const regexLink =/^https?:\/\/(.+\/)+.+$/i
-    // const regexLink=/^(https?:\\/\\/)?'+
-    // '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ 
-    // '((\\d{1,3}\\.){3}\\d{1,3}))'+ 
-    // '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
-    // '(\\?[;&a-z\\d%_.~+=-]*)?'+
-    // '(\\#[-a-z\\d_]*)?$'/
-    return regexLink.test(longUrl)
-}
+function isValidLink(longUrl) {
+    const pattern = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
+    return pattern.test(longUrl);
+  }
 module.exports.isValidLink=isValidLink;
