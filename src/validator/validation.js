5121089d0a4ed2fb12e7bbb6fc32c9e0d9f41be4
anmol <anmolkadam369@gmail.com>
2023-01-19 23:18:55 +0530

    final CODE

--- src/validator/validation.js
+++ src/validator/validation.js
@@ -1,6 +1,5 @@
 
 function isValidLink(longUrl) {
-    // const pattern = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
     const pattern = /^(ftp|http|https):\/\/[^ "]+$/;
     return pattern.test(longUrl);
   }
